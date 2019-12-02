6cd725d7facac502ab6e586dfe71def49d86cac5
wenchao.hao <wenchao.hao@aiads.com>
2019-12-02 18:20:32 +0800

    add Flask's blueprint feature to module of net.web_flask

--- src/bee/net/web_flask/__init__.py
+++ src/bee/net/web_flask/__init__.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, Blueprint
+from bee import config
+from bee.data.guid import Guid
+from bee.data.map import Map
+from bee.bee import banner
 
 __author__ = 'wenchao.hao'
 
@@ -8,11 +12,33 @@ __author__ = 'wenchao.hao'
 net.web_flask package. use flask lib as bee's web framework
 """
 app = Flask(__name__.split(".")[0])
+bapp = Blueprint('bee-' + Guid().string(), __name__)
+print(bapp.name)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@bapp.route('/', methods=['GET', 'POST'])
 def home():
     s = '<h1>Welcome To Bee Web Home</h1>'
     s += "\n"
-    s += "Use Native Flask Framework, enjoy it!"
+    s += "Use Natural Flask Framework, enjoy it!"
     return s
+
+
+class ServerOptions(Map):
+
+    def __init__(self, port: int = 8080, context_path: str = "bee", banner: bool = True, debug: bool = True):
+        self.port = port
+        self.path = context_path
+        self.banner = banner
+        self.debug = debug
+
+
+def start():
+    port = config.get("server.port")
+    path = config.get("server.context-path")
+    opts = ServerOptions(port=port, context_path=path)
+    if opts.debug:
+        print(banner)
+
+    app.register_blueprint(bapp, url_prefix=path)
+    app.run(debug=opts.debug, use_reloader=False, host='0.0.0.0', port=port)
--- tests/test_web_server.py
+++ tests/test_web_server.py
@@ -1,8 +1,10 @@
-from bee.net.web_flask import app
-
+from bee.net.web_flask import bapp
 from bee.net.web_flask.server import start_up
 
 
+@bapp.route('/hello', methods=['GET', 'POST'])
+def hello():
+    s = '<h1>Hello world</h1>'
+    return s
 
-
-start_up(app)
+start_up()
